cd46301737caee46fff5e626184a4e5e226558e3
Nickname Lastname <adada2231wadsa324@gmail.com>
2023-03-14 17:24:55 +0600

    Файл для создания класса индексной страницы

---
+++ webapp/views/base.py
@@ -0,0 +1,41 @@
+from django.db.models import Q
+from django.views.generic import ListView
+from webapp.models.products import Product
+from django.utils.http import urlencode
+from webapp.forms import SearchForm
+
+
+class IndexView(ListView):
+    template_name = 'index.html'
+    model = Product
+    context_object_name = 'products'
+    ordering = ('created_at',)
+    paginate_by = 5
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
+
+    def get_queryset(self):
+        queryset = super().get_queryset().exclude(is_deleted=True).order_by('title', 'category')
+        if self.search_value:
+            query = Q(title__icontains=self.search_value) | Q(description__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=None, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+        return context
+
